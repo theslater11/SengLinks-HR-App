dbc1619d1b8ae1a19a0d9f4f941e07055a34a0d3
Your Name <youremail@example.com>
2024-12-05 15:04:05 -0500

    DID IT!!!

--- metrics.py
+++ metrics.py
@@ -9,11 +9,10 @@ def window_max(data: list, n: int) -> list:
         list[int]: list of maximums from each window (size should be len(data)//6)
     """
     maximums = []
-    listo = []
-    #Creating a list of numbers in a list until n is reached, then a new list is created and stored. 
-    listo.append(data[:n:]) #gotta be a range function
+    #Creating a list of numbers in a list until n is reached, saving that list, then creating a new list.
+    listo = [data[x:x+n] for x in range(0, len(data), n)]
     print(listo)
-    #for loop to return max values from each list in a list of lists
+    #for loop to save max values from each list in a list of lists to list -> maximums
     for size in listo:
         num = 0
         if max(size) > num:
@@ -21,12 +20,22 @@ def window_max(data: list, n: int) -> list:
             maximums.append(num)
     return maximums
 result = window_max([1, 5, 3, 2, 4, 6, 7, 10, 9, 8], 4)
+
 print(result)
     
 
 
 def window_average(data: list, n: int) -> list:
-    pass
+    averages =[]
+    lista = [data[x:x+n] for x in range(0, len(data), n)]
+    for avs in lista:
+        averages.append(sum(avs) // len(avs))
+    return averages
+
+result = window_average([1, 5, 3, 2, 4, 6, 7, 10, 9, 8], 4)
+
+print(result)
+    
 
 
 def window_stddev(data: list, n: int) -> list:
